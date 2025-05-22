7fec63b5778081ef85fcfc88ab41ca138a2bbff7
masaun <masaun@protonmail.com>
2025-05-22 22:33:47 +0900

    [circuit & circuit test + SC test]: Adjust for the SC test -> [Error - UltraHonk]: 'Failed to open data file for writing: ./target/test_wrongProof.proof/proof'

--- test/circuit/Starter.t.sol
+++ test/circuit/Starter.t.sol
@@ -22,22 +22,29 @@ contract StarterTest is Test {
 
     function test_verifyProof() public {
         noirHelper.withInput("x", 1)
-                  .withInput("y", 1)
-                  .withInput("nullifier", bytes32(uint256(0x0d550adf957548876e708338ed4e94a657a3ae4a99538d716faf422679df79d8)))
-                  .withInput("return", bytes32(uint256(0x0d550adf957548876e708338ed4e94a657a3ae4a99538d716faf422679df79d8)));
+                  .withInput("y", 1);
+                  //.withInput("nullifier", bytes32(uint256(0x0d550adf957548876e708338ed4e94a657a3ae4a99538d716faf422679df79d8)));
+                  //.withInput("return", bytes32(uint256(0x0d550adf957548876e708338ed4e94a657a3ae4a99538d716faf422679df79d8)));
         (bytes32[] memory publicInputs, bytes memory proof) = noirHelper.generateProof("test_verifyProof", 2);
         starter.verifyEqual(proof, publicInputs);
     }
 
     function test_wrongProof() public {
         noirHelper.clean();
-        noirHelper.withInput("x", 3)
-                  .withInput("y", 5)
-                  .withInput("nullifier", bytes32(uint256(0x0d550adf957548876e708338ed4e94a657a3ae4a99538d716faf422679df79d8)))
-                  .withInput("return", bytes32(uint256(0x0d550adf957548876e708338ed4e94a657a3ae4a99538d716faf422679df79d8)));
+        noirHelper.withInput("x", 5)
+                  .withInput("y", 5);
+                  //.withInput("nullifier", bytes32(uint256(0x0d550adf957548876e708338ed4e94a657a3ae4a99538d716faf422679df79d8)));
+                  //.withInput("return", bytes32(uint256(0x0d550adf957548876e708338ed4e94a657a3ae4a99538d716faf422679df79d8)));
         (bytes32[] memory publicInputs, bytes memory proof) = noirHelper.generateProof("test_wrongProof", 2);
+
+        /// @dev - Create a fake public input, which should fail because the public input is wrong
+        bytes32[] memory fakePublicInputs = new bytes32[](2);
+        fakePublicInputs[0] = publicInputs[0];
+        fakePublicInputs[1] = bytes32(uint256(0xddddd));  // @dev - This is wrong publicInput ("nulifieir")
+
+        /// @dev - Verify the proof, which should be reverted
         vm.expectRevert();
-        starter.verifyEqual(proof, publicInputs);
+        starter.verifyEqual(proof, fakePublicInputs);
     }
 
     // function test_all() public {
