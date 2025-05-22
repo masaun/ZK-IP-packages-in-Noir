05ec072244800337a999412896b7ff6856598b0f
masaun <masaun@protonmail.com>
2025-05-22 22:06:39 +0900

    [circuit]: Modify the return value from 'RevealedData' to 'Field' + [SC test]: Implement the StarterTest

--- test/circuit/Starter.t.sol
+++ test/circuit/Starter.t.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.8.17;
 
-import "../contract/Starter.sol";
-import "../circuits/target/contract.sol";
+import { HonkVerifier } from "../../contracts/circuit/ultra-verifier/plonk_vk.sol";
+import { Starter } from "../../contracts/circuit/Starter.sol";
+//import "../circuits/target/contract.sol";
 import "forge-std/console.sol";
 
 import "forge-std/Test.sol";
@@ -10,24 +11,30 @@ import {NoirHelper} from "foundry-noir-helper/NoirHelper.sol";
 
 contract StarterTest is Test {
     Starter public starter;
-    UltraVerifier public verifier;
+    HonkVerifier public verifier;
     NoirHelper public noirHelper;
 
     function setUp() public {
         noirHelper = new NoirHelper();
-        verifier = new UltraVerifier();
+        verifier = new HonkVerifier();
         starter = new Starter(verifier);
     }
 
     function test_verifyProof() public {
-        noirHelper.withInput("x", 1).withInput("y", 1).withInput("return", 1);
+        noirHelper.withInput("x", 1)
+                  .withInput("y", 1)
+                  .withInput("nullifier", bytes32(uint256(0x0d550adf957548876e708338ed4e94a657a3ae4a99538d716faf422679df79d8)))
+                  .withInput("return", bytes32(uint256(0x0d550adf957548876e708338ed4e94a657a3ae4a99538d716faf422679df79d8)));
         (bytes32[] memory publicInputs, bytes memory proof) = noirHelper.generateProof("test_verifyProof", 2);
         starter.verifyEqual(proof, publicInputs);
     }
 
     function test_wrongProof() public {
         noirHelper.clean();
-        noirHelper.withInput("x", 1).withInput("y", 5).withInput("return", 5);
+        noirHelper.withInput("x", 3)
+                  .withInput("y", 5)
+                  .withInput("nullifier", bytes32(uint256(0x0d550adf957548876e708338ed4e94a657a3ae4a99538d716faf422679df79d8)))
+                  .withInput("return", bytes32(uint256(0x0d550adf957548876e708338ed4e94a657a3ae4a99538d716faf422679df79d8)));
         (bytes32[] memory publicInputs, bytes memory proof) = noirHelper.generateProof("test_wrongProof", 2);
         vm.expectRevert();
         starter.verifyEqual(proof, publicInputs);
